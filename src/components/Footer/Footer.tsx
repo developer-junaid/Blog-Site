95957731dcc0b491f9269bae032a0327ead86228
developer-junaid <qjunaid623@gmail.com>
2021-04-04 09:47:34 +0500

    Styled Homepage
    NEXT: Category Page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,22 +7,24 @@ import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 // Navbar Containing Links to pages
 export default function Footer() {
   return (
-    <footer className="footer">
-      <hr />
-      <div className="icons-container">
-        <a href="#">
-          <FaFacebook />
-        </a>
-        <a href="#">
-          <FaLinkedin />
-        </a>
-        <a href="#">
-          <FaGithub />
-        </a>
-      </div>
-      <p>
-        Developed by: <span>Junaid Qureshi</span>
-      </p>
-    </footer>
+    <div className="footer-wrapper">
+      <footer className="footer">
+        <hr />
+        <div className="icons-container">
+          <a href="https://www.facebook.com/JunaidQureshi6/">
+            <FaFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/developer-junaid/">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/developer-junaid">
+            <FaGithub />
+          </a>
+        </div>
+        <p>
+          Developed by: <span>Junaid Qureshi</span>
+        </p>
+      </footer>
+    </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,78 +1,91 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import "./index.css";
 
 export default () => {
   return (
     <Layout>
-      {/* Hero Section */}
-      <section className="hero-section">
-        <div className="large-img"></div>
-        <div className="small-images">
-          <div className="small-img"></div>
-          <div className="small-img"></div>
-          <div className="small-img"></div>
-          <div className="small-img"></div>
-        </div>
-      </section>
-
-      {/* Categories Section */}
-      <section className="categories-section">
-        <h3>Categories</h3>
-        <div className="category-card">
-          <button>Gadgets</button>
-        </div>
-        <div className="category-card">
-          <button>Marketing</button>
-        </div>
-        <div className="category-card">
-          <button>Trends</button>
-        </div>
-      </section>
-
-      {/* Blogs Section */}
-      <section className="blogs-section">
-        <h3>From The Blog</h3>
-        <div className="blog-card">
-          <div className="card-img"></div>
-          <div className="card-content">
-            <p className="card-title">
-              What 3 years of android development taught me the hard way
-            </p>
-            <p className="card-date">May 15 2018</p>
-            <p className="card-tag">Trends</p>
+      <div className="homepage-container">
+        {/* Hero Section */}
+        <section className="hero-section">
+          <div className="large-img">All about tech !</div>
+          <div className="small-images">
+            <div className="small-img" id="small-img-1"></div>
+            <div className="small-img" id="small-img-2"></div>
+            <div className="small-img" id="small-img-3"></div>
+            <div className="small-img" id="small-img-4"></div>
           </div>
-        </div>
-        <div className="blog-card">
-          <div className="card-img"></div>
-          <div className="card-content">
-            <p className="card-title">
-              What 3 years of android development taught me the hard way
-            </p>
-            <p className="card-date">May 15 2018</p>
-            <p className="card-tag">Trends</p>
-          </div>
-        </div>
-        <div className="blog-card">
-          <div className="card-img"></div>
-          <div className="card-content">
-            <p className="card-title">
-              What 3 years of android development taught me the hard way
-            </p>
-            <p className="card-date">May 15 2018</p>
-            <p className="card-tag">Trends</p>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      {/* Newsletter Section */}
-      <section className="newsletter-section">
-        <h3>Newsletter</h3>
-        <p>Enter your email address below to subscribe to my newsletter</p>
-        <form>
-          <input type="text" placeholder="Your email address.." />
-          <button type="submit">Subscribe</button>
-        </form>
-      </section>
+        {/* Categories Section */}
+        <section className="categories-section">
+          <div className="heading">
+            <h3>Categories</h3>
+            <div className="underline"></div>
+          </div>
+
+          <div className="category-card" id="gadgets-category-card">
+            <button>Gadgets</button>
+          </div>
+          <div className="category-card" id="marketing-category-card">
+            <button>Marketing</button>
+          </div>
+          <div className="category-card" id="trends-category-card">
+            <button>Trends</button>
+          </div>
+        </section>
+
+        {/* Blogs Section */}
+        <section className="blogs-section">
+          <div className="heading">
+            <h3>From The Blog</h3>
+            <div className="underline"></div>
+          </div>
+          <div className="blog-card">
+            <div className="card-img" id="card-img-1"></div>
+            <div className="card-content">
+              <p className="card-title">
+                What 3 years of android development taught me the hard way
+              </p>
+              <p className="card-date">May 15 2018</p>
+              <p className="card-tag">Trends</p>
+            </div>
+          </div>
+          <div className="blog-card">
+            <div className="card-img" id="card-img-2"></div>
+            <div className="card-content">
+              <p className="card-title">
+                In defense of being on the phone all the damn time 24 hours
+              </p>
+              <p className="card-date">May 15 2018</p>
+              <p className="card-tag">Trends</p>
+            </div>
+          </div>
+          <div className="blog-card">
+            <div className="card-img" id="card-img-3"></div>
+            <div className="card-content">
+              <p className="card-title">
+                Infusing Tesla’s future with Mustang’s pride and passion
+              </p>
+              <p className="card-date">May 15 2018</p>
+              <p className="card-tag">Trends</p>
+            </div>
+          </div>
+        </section>
+
+        {/* Newsletter Section */}
+        <section className="newsletter-section">
+          <div className="heading">
+            <h3>Newsletter</h3>
+            <div className="underline"></div>
+          </div>
+          <p>Enter your email address below to subscribe to my newsletter</p>
+          <form>
+            <input type="email" placeholder="Your email address.." />
+            <button type="submit">Subscribe</button>
+          </form>
+        </section>
+      </div>
     </Layout>
   );
 };
