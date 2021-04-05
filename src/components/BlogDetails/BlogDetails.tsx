829fe75f2573c8ec89e10bb1afeb809cd128cc4d
developer-junaid <qjunaid623@gmail.com>
2021-04-05 11:07:55 +0500

    Rendered Rich Text
    NEXT: Category Section

--- src/components/BlogDetails/BlogDetails.tsx
+++ src/components/BlogDetails/BlogDetails.tsx
@@ -4,12 +4,14 @@ import Layout from "./../Layout/Layout";
 import Newsletter from "./../Landing/Newsletter/Newsletter";
 import Heading from "../Heading/Heading";
 import Author from "./Author/Author";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
 interface blogDetailsPropsType {
   title: string;
   createdAt: string;
   imageSrc: string;
-  content: {};
+  content: string;
   likes: number;
   author: string;
 }
@@ -22,7 +24,29 @@ const BlogDetails: React.FC<blogDetailsPropsType> = ({
   likes,
   author,
 }) => {
-  console.warn(content);
+  const contentJSON = JSON.parse(content);
+  console.log(contentJSON);
+
+  // Options
+  const RICHTEXT_OPTIONS = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => {
+        return (
+          <p>
+            {children}
+            <br />
+          </p>
+        );
+      },
+      [INLINES.HYPERLINK]: (node, children) => {
+        return (
+          <a href={node.data.uri} style={{ color: "#695384" }}>
+            {children}
+          </a>
+        );
+      },
+    },
+  };
 
   return (
     <Layout>
@@ -32,29 +56,7 @@ const BlogDetails: React.FC<blogDetailsPropsType> = ({
         </div>
         <Heading title={title} style={{ width: "60vw", margin: "3rem auto" }} />
         <div className="content">
-          <div>
-            More off this less hello salamander lied porpoise much over tightly
-            circa horse taped so innocuously outside crud mightily rigorous plot
-            life. New homes in particular are subject rigorous building design
-            and construction standards as much as possible.
-          </div>{" "}
-          <br />
-          As you’re tapping. scrolling, and swiping on your phone, you probably
-          don’t give much thought to the fact that your apps are consuming
-          electricity — just that they’re chipping away at your battery life.
-          It’s engaging. It’s hopeful. It pushes the plot forward. Before you
-          click away, disappointed that I brought up something as practical and
-          boring as time management, hear me out. My intent is not to crush your
-          spirits.
-          <br />
-          <br /> <b>Current Mobile Cloud</b>
-          <br />
-          <br /> The average U.S. iPhone owner uses 1–2 gigabytes of LTE data
-          per month, according to some studies. That data comes at an energy
-          cost that’s opaque, because we can’t see it directly. You can’t look
-          to your peers in the industry, because all they’re going to tell you
-          is that they are killing it, crushing it, changing the world. Example:
-          Journey towards design perfection
+          {documentToReactComponents(contentJSON, RICHTEXT_OPTIONS)}
         </div>
       </div>
       <Author author={author} likes={likes} />
