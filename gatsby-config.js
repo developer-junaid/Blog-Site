54fca5e3888ff71167cde2a68340e9ac28d304b2
developer-junaid <qjunaid623@gmail.com>
2021-04-09 09:12:28 +0500

    Added MetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,26 @@ dotenv.config();
 
 module.exports = {
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `TechBerg Blog`,
+        short_name: `TechBerg`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "./src/assets/images/blog.png",
+        lang: `en`,
+        description: `Tech Berg Blog App template using Gatsby, Contentful and Netlify`,
       },
     },
   ],
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import "./layout.css";
+import { Helmet } from "react-helmet";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,6 +11,14 @@ interface LayoutProps {
 export default ({ children }: LayoutProps) => {
   return (
     <div className="layout">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>TechBerg</title>
+        <meta
+          name="description"
+          content="Tech Berg Blog Website Template using Gatsby"
+        />
+      </Helmet>
       <Navbar />
       <div className="children">{children}</div>
       <Footer />
